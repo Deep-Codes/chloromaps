7ba1372f614d764c7bbaf69b5716aecd39451b3e
Deepankar Bhade <deep14bhade@gmail.com>
2021-05-04 01:07:41 +0530

    feat: mvp of legend controls

--- components/LegendControls.tsx
+++ components/LegendControls.tsx
@@ -1,17 +1,50 @@
-import getColorUsed from '@/lib/getColorUsed';
 import { mapAtom } from '@/store/map.store';
 import { MapStoreType } from '@/typings/map.store';
+import { Input } from '@geist-ui/react';
+import { Eye, XCircle, EyeOff } from '@geist-ui/react-icons';
 import { useAtom } from 'jotai';
 import React from 'react';
 import InputLabel from './InputLabel';
 
 const LegendControls = () => {
     const [map, setMap] = useAtom<MapStoreType>(mapAtom);
-    const dt = getColorUsed(map.mapData);
     return (
-        <div>
+        <div className="ctx">
             <InputLabel text="Legend Settings" />
-            <div className="flex justify-between">fill input cross</div>
+            {map.legendData.map((dt) => (
+                <div key={dt.fill} className="flex-center justify-between m-1">
+                    <div className="icon-btn flex-center pointer">
+                        {dt.hide ? <EyeOff size={20} /> : <Eye size={20} />}
+                    </div>
+
+                    <div style={{ backgroundColor: dt.fill }} className="box" />
+                    <Input size="mini" placeholder="Legend Value" value={dt.text} />
+                    <div className="icon-btn flex-center pointer">
+                        <XCircle size={20} />
+                    </div>
+                </div>
+            ))}
+            <style jsx>{`
+                .ctx {
+                    width: 250px;
+                }
+                .box {
+                    width: 20px;
+                    height: 20px;
+                    border-radius: 2px;
+                    margin-right: 5px;
+                }
+                .icon-btn {
+                    opacity: 0.5;
+                }
+                .icon-btn:hover {
+                    opacity: 1;
+                }
+                .m-1 {
+                    margin-top: 0.5rem;
+                    margin-bottom: 0.5rem;
+                }
+            `}</style>
         </div>
     );
 };
