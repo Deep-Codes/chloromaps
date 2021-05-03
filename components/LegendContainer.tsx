f2beeead3d8c9852344723cafce430afcffba522
Deepankar Bhade <deep14bhade@gmail.com>
2021-05-04 03:29:36 +0530

    fix: svg increases based on calcH

--- components/LegendContainer.tsx
+++ components/LegendContainer.tsx
@@ -16,25 +16,28 @@ if height = 30 then viewBox="0 0 364 30"
 
 */
 
-const LegendContainer = ({ data }: Props) => (
-    <svg
-        id="legend"
-        width="364"
-        height="380"
-        viewBox="0 0 364 380"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg">
-        <g id="legend-group">
-            {data.map((dt: LegendData, i: number) => (
-                <svg key={dt.fill} height={30} y={`${30 * i + 5}`}>
-                    <rect x="55" y="5" width="20" height="20" fill={dt.fill} />
-                    <text fontFamily="Roboto" fill="white" x="85" y="20">
-                        {dt.text}
-                    </text>
-                </svg>
-            ))}
-        </g>
-    </svg>
-);
+const LegendContainer = ({ data }: Props) => {
+    const calcH = data.length * 40;
+    return (
+        <svg
+            id="legend"
+            width="364"
+            height={calcH}
+            viewBox={`0 0 364 ${calcH}`}
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <g id="legend-group">
+                {data.map((dt: LegendData, i: number) => (
+                    <svg key={dt.fill} height={30} y={`${30 * i + 5}`}>
+                        <rect x="55" y="5" width="20" height="20" fill={dt.fill} />
+                        <text fontFamily="Roboto" fill="white" x="85" y="20">
+                            {dt.text}
+                        </text>
+                    </svg>
+                ))}
+            </g>
+        </svg>
+    );
+};
 
 export default LegendContainer;
