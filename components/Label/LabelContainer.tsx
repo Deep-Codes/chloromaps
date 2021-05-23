5e820d3f7504c04c0d822470dec29bb40d22c8b8
Deepankar Bhade <deep14bhade@gmail.com>
2021-05-23 23:03:10 +0530

    refactor: replace label ctx with table

--- components/Label/LabelContainer.tsx
+++ components/Label/LabelContainer.tsx
@@ -3,13 +3,14 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import { labelAtom } from '@/store/label.store';
 import { LabelStoreType } from '@/typings/map.store';
-import { Input } from '@geist-ui/react';
+import { Input, useTheme } from '@geist-ui/react';
 import { EyeOff, Eye, XCircle } from '@geist-ui/react-icons';
 import { useAtom } from 'jotai';
 import React from 'react';
 import InputLabel from '../InputLabel';
 
 const LabelContainer: React.FC = () => {
+    const theme = useTheme();
     const [label, setLabel] = useAtom<LabelStoreType>(labelAtom);
     const removeLabel = (i: number) => {
         label.data.splice(i, 1);
@@ -53,24 +54,40 @@ const LabelContainer: React.FC = () => {
     return (
         <div className="ctx">
             <InputLabel text="Label Controls" />
-            {label.data.map((d, i) => (
-                <div key={d.id} className="flex justify-between box">
-                    <div
-                        className="icon-btn flex-center pointer"
-                        onClick={() => toggleHideLabel(i)}>
-                        {d.hide ? <EyeOff size={20} /> : <Eye size={20} />}
-                    </div>
-                    <Input
-                        size="mini"
-                        placeholder="Label Value"
-                        value={d.text}
-                        onChange={(e) => updateTextLabel(i, e.target.value)}
-                    />
-                    <div className="icon-btn flex-center pointer" onClick={() => removeLabel(i)}>
-                        <XCircle size={20} />
-                    </div>
-                </div>
-            ))}
+            <table className="table">
+                <tr>
+                    <th>Hide</th>
+                    <th>Input</th>
+                    <th>Remove</th>
+                </tr>
+                {label.data.map((d, i) => (
+                    <tr key={d.id}>
+                        <td>
+                            <div
+                                className="icon-btn flex-center pointer"
+                                onClick={() => toggleHideLabel(i)}>
+                                {d.hide ? <EyeOff size={20} /> : <Eye size={20} />}
+                            </div>
+                        </td>
+                        <td>
+                            <Input
+                                size="mini"
+                                placeholder="Label Value"
+                                value={d.text}
+                                onChange={(e) => updateTextLabel(i, e.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <div
+                                className="icon-btn flex-center pointer"
+                                onClick={() => removeLabel(i)}>
+                                <XCircle size={20} />
+                            </div>
+                        </td>
+                    </tr>
+                ))}
+            </table>
+
             <style jsx>{`
                 .ctx {
                     margin: 20px 0;
@@ -84,6 +101,62 @@ const LabelContainer: React.FC = () => {
                 }
                 .icon-btn:hover {
                     opacity: 1;
+                }
+                table {
+                    border-collapse: separate;
+                    border-spacing: 0;
+                    width: 100%;
+                }
+                tr:hover {
+                    background-color: ${theme.palette.accents_1};
+                }
+                table thead th td {
+                    height: 2.5rem;
+                }
+                table tbody tr td {
+                    height: 3.125rem;
+                }
+                table th,
+                table td {
+                    padding: 0 0.625rem;
+                    text-align: left;
+                }
+                table th {
+                    height: 2.5rem;
+                    color: ${theme.palette.accents_5};
+                    font-size: 0.875rem;
+                    font-weight: 400;
+                    letter-spacing: 0;
+                    background: ${theme.palette.accents_1};
+                    border-bottom: 1px solid ${theme.palette.accents_2};
+                    border-top: 1px solid ${theme.palette.accents_2};
+                }
+                table th:nth-child(1) {
+                    border-bottom: 1px solid ${theme.palette.accents_2};
+                    border-left: 1px solid ${theme.palette.accents_2};
+                    border-radius: 4px 0 0 4px;
+                    border-top: 1px solid ${theme.palette.accents_2};
+                }
+                table th:last-child {
+                    border-bottom: 1px solid ${theme.palette.accents_2};
+                    border-radius: 0 4px 4px 0;
+                    border-right: 1px solid ${theme.palette.accents_2};
+                    border-top: 1px solid ${theme.palette.accents_2};
+                }
+                table tr td {
+                    color: var(--accents6);
+                    font-size: 0.875rem;
+                    height: 2.5rem;
+                }
+                table td:nth-child(1) {
+                    border-left: 1px solid transparent;
+                }
+                table tr:not(:last-of-type) td {
+                    border-bottom: 1px solid ${theme.palette.accents_2};
+                }
+                .table-wrapper {
+                    margin: 30px 0;
+                    overflow-x: scroll;
                 }
             `}</style>
         </div>
