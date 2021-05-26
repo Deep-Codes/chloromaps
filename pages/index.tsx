897d08dc62367f7bb46664e944a5d64a6d6a70b2
Deepankar Bhade <deep14bhade@gmail.com>
2021-05-27 00:56:11 +0530

    feat: landing country cards -> grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,9 @@
 import React from 'react';
 import MainLayout from '@/layouts/MainLayout';
-import { Card, Link, Row, Spacer } from '@geist-ui/react';
+import { Spacer } from '@geist-ui/react';
 import LandingWorldMap from '@/components/Landing/LandingWorldMap';
 import FeatureContainer from '@/components/Landing/FeatureContainer';
-
-export const mapDt = [
-    {
-        name: 'World',
-        text: '180+ Countries Based on Robinson Projection.',
-        link: '/world'
-    },
-    {
-        name: 'India',
-        text: 'All Indian States Based on Mercator Projection.',
-        link: '/india'
-    },
-    {
-        name: 'Usa',
-        text: 'All 50 States Based on Lambert Azimuthal Equal-area projection.',
-        link: '/usa'
-    },
-    {
-        name: 'Europe',
-        text: 'Both EU + Non-EU , Based on Robinson Projection.',
-        link: '/europe'
-    },
-    {
-        name: 'Russia',
-        text: 'Russian Federation, Based on Mercator Projection.',
-        link: '/russia'
-    },
-    {
-        name: 'Sweden',
-        text: 'All States included , Based on Mercator Projection.',
-        link: '/sweden'
-    }
-];
+import MapShowContainer from '@/components/Landing/MapShowContainer';
 
 const Home: React.FC = () => (
     <MainLayout>
@@ -48,19 +16,8 @@ const Home: React.FC = () => (
             <Spacer y={5} />
             <h1 className="mx-auto">Try out These Maps!</h1>
             <Spacer y={2} />
-            <Row style={{ flexWrap: 'wrap' }} justify="space-between">
-                {mapDt.map((el) => (
-                    <Card key={el.name} width="320px" style={{ marginBottom: '20px' }}>
-                        <h2>{el.name.toUpperCase()}</h2>
-                        <p>{el.text}</p>
-                        <Card.Footer>
-                            <Link color href={el.link}>
-                                Create
-                            </Link>
-                        </Card.Footer>
-                    </Card>
-                ))}
-            </Row>
+            <MapShowContainer />
+            <Spacer y={10} />
         </div>
     </MainLayout>
 );
