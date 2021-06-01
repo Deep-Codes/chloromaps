3aeded8555f66bb443ede9ab030efe7f2297efc3
Deepankar Bhade <deep14bhade@gmail.com>
2021-06-01 08:22:06 +0530

    feat: download as svg

--- lib/downloadMap.ts
+++ lib/downloadMap.ts
@@ -1,7 +1,23 @@
 import { saveSvgAsPng } from 'save-svg-as-png';
 
-const downloadMap = (id: string) => {
-  saveSvgAsPng(document.getElementById(id), 'map.png');
+const downloadMap = (id: string, type: 'png' | 'svg') => {
+    const mapEl = document.getElementById(id);
+    if (type === 'png') {
+        saveSvgAsPng(document.getElementById(id), 'map.png');
+    }
+    if (type === 'svg') {
+        if (mapEl) {
+            const svgDataString = mapEl.outerHTML;
+            const element = document.createElement('a');
+            const file = new Blob([(svgDataString)], {
+                type: 'text/plain;charset=utf-8'
+            });
+            element.href = URL.createObjectURL(file);
+            element.download = 'map.svg';
+            document.body.appendChild(element);
+            element.click();
+        }
+    }
 };
 
-export default downloadMap
+export default downloadMap;
