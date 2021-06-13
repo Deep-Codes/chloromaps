fa0615146265e108fc549619fd8aeee15de3a4fc
Deepankar Bhade <deep14bhade@gmail.com>
2021-06-13 13:37:21 +0530

    feat: displays data and map

--- labs/utils/getPaletteData.ts
+++ labs/utils/getPaletteData.ts
@@ -2,12 +2,8 @@ import Gradient from 'javascript-color-gradient';
 
 const colorGradient = new Gradient();
 
-const getPaletteData = (count: number) => {
-    const color1 = '#f9d56e';
-    const color2 = '#ff1e56';
-
-    const paletteData = colorGradient.setGradient(color1, color2).setMidpoint(count).getArray();
-
+const getPaletteData = (count: number , colorList: string[]) => {
+    const paletteData = colorGradient.setGradient(...colorList).setMidpoint(count).getArray();
     return paletteData;
 };
 
--- pages/labs/index.tsx
+++ pages/labs/index.tsx
@@ -8,19 +8,28 @@ import MainLayout from '@/layouts/MainLayout';
 import UsaMap from 'labs/usa.map';
 import React from 'react';
 
+interface MapLabData {
+    fill: string;
+    code: string;
+    hide: boolean;
+    val: number;
+}
+
 const Lab = () => {
-    const [data, setData] = React.useState();
+    const [data, setData] = React.useState<MapLabData[]>([]);
     React.useEffect(() => {
         const count = 5;
+        const colorList = ['#f1f3f8', '#393b44'];
         const randData = createRandomData(UsaStateCodes);
         const sortRandData = sortObject(randData);
         const legendArr = getLegendList(sortRandData, count);
-        const paletteArr = getPaletteData(count);
-        const mapData = getMapData({
+        const paletteArr = getPaletteData(count, colorList);
+        const mapData: MapLabData[] = getMapData({
             sortedData: sortRandData,
             legendList: legendArr,
             paletteData: paletteArr
         });
+        setData(mapData);
         mapData.forEach((e) => {
             const el = document.getElementById(e.code);
             if (el) {
@@ -32,13 +41,36 @@ const Lab = () => {
     return (
         <MainLayout>
             <div className="flex justify-between container items-start">
+                <div className="tb-ctx">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Code</th>
+                                <th>Name</th>
+                                <th>Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((e) => (
+                                <tr key={e.code}>
+                                    <td>{e.code}</td>
+                                    {/* @ts-ignore */}
+                                    <td>{UsaStateCodes[e.code]}</td>
+                                    <td>{e.val}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
                 <UsaMap />
             </div>
             <style jsx>{`
                 .items-start {
+                    margin: 40px 0;
                     align-items: flex-start;
                 }
-                table {
+                .tb-ctx {
                     width: 300px;
                     overflow-y: scroll;
                     height: 80vh;
